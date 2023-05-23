6ed8bab105206da42d9023c6f90640b296a44a6f
kolobee <kolobaev.m@mail.ru>
2023-05-23 14:03:05 +0300

    Calculator

--- calcul/calcul/Program.cs
+++ calcul/calcul/Program.cs
@@ -35,7 +35,9 @@ namespace calcul
                     result = num1 * num2;
                     break;
                 case '/':
-                    result = num1 * num2;
+                    if (num2 != 0)
+                        result = num1 * num2;
+                    else Console.WriteLine("Делитель не может быть 0!");
                     break;
 
                 default:
